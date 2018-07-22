17961977b1d285e6d68793cb7da2aad6eb0cb402
m0cchi <boom.boom.planet@gmail.com>
2018-07-22 15:32:15 +0900

    fixed

--- wait_for_tcp.py
+++ wait_for_tcp.py
@@ -13,22 +13,20 @@ host = argv[1]
 port = int(argv[2])
 timeout = int(argv[3])
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ok = False
 start = time()
 while not ok:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         client.connect((host, port))
         ok = True
     except ConnectionRefusedError:
         pass
     finally:
-        if ok:
-            client.close()
-        else:
-            sleep(1)
-        elapsed_time = time() - start
-        if elapsed_time >= timeout:
-            break
+        client.close()
+        if not ok:
+            elapsed_time = time() - start
+            if elapsed_time >= timeout:
+                break
 
 exit(0 if ok else 1)
